db154957a2168bf2544dc1f75e3f0eb3f572c49c
Deepak7510 <deepakkumaryadav75100@gmail.com>
2025-05-22 13:42:31 +0530

    all upload file

--- server/controllers/media/media-controller.js
+++ server/controllers/media/media-controller.js
@@ -5,13 +5,19 @@ import {
 import { ApiError } from "../../utils/api-error.js";
 import { ApiResponse } from "../../utils/api-response.js";
 import asyncHandler from "../../utils/async-handler.js";
-
+import fs from "fs";
 export const uploadMedia = asyncHandler(async (req, res, next) => {
   const filePath = req.file.path;
   if (!filePath) {
     return next(new ApiError(403, "Somthing went worng."));
   }
   const result = await uploadMediaToCloudinary(filePath);
+  if (result) {
+    if (fs.existsSync(filePath)) {
+      fs.unlinkSync(filePath);
+    }
+  }
+
   res
     .status(200)
     .json(new ApiResponse(200, "File uploaded successfully", result));
